0b59c61fdd756bf686ec8e11623731873a11e9f8
stanley <stanleyanosike@gmail.com>
2022-05-02 12:29:11 -0700

    my fylo dark commit.

---
+++ file.js
@@ -0,0 +1,10 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+document.getElementsByClassName
+document.getElementById("rounded").onclick = function() {
+  
+  document.getElementById("circle").style.display = "block";
+}
+
